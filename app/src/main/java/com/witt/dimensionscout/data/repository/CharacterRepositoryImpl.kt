7c361a9de4092ad2f986f2c60259575bc165de27
witt <chris.w.witt@gmail.com>
2026-05-14 09:15:30 -0400

    CW - Adding in memory caching for responses.

--- app/src/main/java/com/witt/dimensionscout/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/witt/dimensionscout/data/repository/CharacterRepositoryImpl.kt
@@ -5,42 +5,48 @@ import com.witt.dimensionscout.R
 import com.witt.dimensionscout.data.remote.CharacterApiService
 import com.witt.dimensionscout.data.remote.dto.RMResponse
 import com.witt.dimensionscout.domain.repository.CharacterRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import okio.IOException
 import retrofit2.HttpException
 import java.net.HttpURLConnection.HTTP_BAD_REQUEST
 import java.net.HttpURLConnection.HTTP_NOT_FOUND
 
 class CharacterRepositoryImpl(private val apiService: CharacterApiService) : CharacterRepository {
-    override suspend fun getCharacters(query: String, page: Int): RMResponse =
-        withContext(Dispatchers.IO) {
-            try {
+
+    private lateinit var cachedCharacters: MutableMap<Pair<String, Int>, RMResponse>
+    override suspend fun getCharacters(query: String, page: Int): RMResponse {
+        try {
+            val cachedResponse = cachedCharacters.get(Pair(query, page))
+            if (cachedResponse != null) {
+                return cachedResponse
+            } else {
                 val dto = apiService.getCharacters(query, page)
                 val results = dto.results ?: emptyList()
-                RMResponse.Success(
+                val response = RMResponse.Success(
                     data = results.map { it.toDomain() },
                     hasNextPage = dto.info != null && dto.info.next != null
                 )
-            } catch (e: IOException) {
-                Log.e(TAG, "IOException calling getCharacters: ${e.message}")
-                RMResponse.Error(R.string.error_io_exception)
-            } catch (e: HttpException) {
-                Log.e(
-                    TAG,
-                    "HttpException calling getCharacters with code ${e.code()}: ${e.message}"
-                )
-                when (e.code()) {
-                    HTTP_NOT_FOUND -> RMResponse.Success(emptyList(), hasNextPage = false)
-                    HTTP_BAD_REQUEST -> RMResponse.Error(R.string.error_http_400)
-                    429 -> RMResponse.Error(R.string.error_http_429)
-                    else -> RMResponse.Error(R.string.error_http_exception)
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "Exception calling getCharacters: ${e.message}")
-                RMResponse.Error(R.string.error_generic_exception)
+                cachedCharacters[Pair(query, page)] = response
+                return response
             }
+        } catch (e: IOException) {
+            Log.e(TAG, "IOException calling getCharacters: ${e.message}")
+            return RMResponse.Error(R.string.error_io_exception)
+        } catch (e: HttpException) {
+            Log.e(
+                TAG,
+                "HttpException calling getCharacters with code ${e.code()}: ${e.message}"
+            )
+            return when (e.code()) {
+                HTTP_NOT_FOUND -> RMResponse.Success(emptyList(), hasNextPage = false)
+                HTTP_BAD_REQUEST -> RMResponse.Error(R.string.error_http_400)
+                429 -> RMResponse.Error(R.string.error_http_429)
+                else -> RMResponse.Error(R.string.error_http_exception)
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception calling getCharacters: ${e.message}")
+            return RMResponse.Error(R.string.error_generic_exception)
         }
+    }
 
     companion object {
         private const val TAG = "CharacterRepositoryImpl"
