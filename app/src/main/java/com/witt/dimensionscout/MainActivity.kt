887e1b0d93179d0b2c7d3a4f5a9d7e0e321dde0a
witt <chris.w.witt@gmail.com>
2026-05-15 12:49:06 -0400

    CW - Fixing redundant scaffold.

--- app/src/main/java/com/witt/dimensionscout/MainActivity.kt
+++ app/src/main/java/com/witt/dimensionscout/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.witt.dimensionscout.navigation.AppNavHost
 import com.witt.dimensionscout.ui.theme.DimensionScoutTheme
 
@@ -19,9 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DimensionScoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavHost(Modifier.padding(innerPadding))
-                }
+                AppNavHost()
             }
         }
     }
